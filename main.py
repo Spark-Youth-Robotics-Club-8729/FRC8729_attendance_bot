d97520e2d17705f8adb59b6ebf25a58c1f32ac0e
Lucas Jin <kblazer20@gmail.com>
2024-11-11 22:48:48 -0500

    Got writing to spreadsheet working

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ import time
 import sqlite3
 import datetime
 from dotenv import load_dotenv
-#testing
+from google.oauth2.service_account import Credentials
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 
 # GETTING VALUES FROM .env
 load_dotenv(".env")
@@ -16,6 +18,46 @@ TOKEN: str = os.getenv("TOKEN")
 ID: int = os.getenv("ID")
 # specific channel id for the channel where bot messages are sent
 CHAN: int = int(os.getenv("CHAN"))
+# spreadsheet id for accessing api
+SPREADSHEETID: str = os.getenv("SPREADSHEETID")
+
+# load credentials for google sheet
+creds = Credentials.from_service_account_file(
+    'timesheet-2024-2025-6a701c48c246.json',
+    scopes=["https://www.googleapis.com/auth/spreadsheets"]
+)
+
+# where to start making sheet
+RANGE_NAME = 'Sheet1!A1'
+
+service = build('sheets', 'v4', credentials=creds)
+
+def write_to_google_sheet(data):
+    try:
+        # prepare data (list of rows (each individual row is a list))
+        body = {
+            'values': data  # data is a list of lists where each list is a row
+        }
+
+        # update spreadsheet
+        result = service.spreadsheets().values().update(
+            spreadsheetId=SPREADSHEETID,
+            range=RANGE_NAME,
+            valueInputOption="RAW",
+            body=body
+        ).execute()
+        print(f"Data written to sheet: {result}")
+    except HttpError as err:
+        print(f"Error: {err}")
+
+# TESTING DATA FOR RN
+users_data = [
+    ["Alice", "Software", "Clocked In"],
+    ["Bob", "Mechanical", "Clocked Out"],
+    ["Charlie", "Business", "Clocked In"]
+]
+
+write_to_google_sheet(users_data)
 
 
 # CLASS DEFINITIONS + INITIALIZATIONS
